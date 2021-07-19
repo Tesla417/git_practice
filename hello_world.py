d4b8413ef5a60267c0a41b0cccc7a32f8795e237
Tesla417 <15251898023@163.com>
2021-07-19 10:48:22 +0800

    Started project.

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+#########################################################################
+# File Name: hello_world.py
+# Description: 
+# Author: C.W.Wang
+# mail: wangcw@ihep.ac.cn
+# Created Time: 一  7/19 10:38:46 2021
+#########################################################################
+
+print("Hello Git world!")
+
